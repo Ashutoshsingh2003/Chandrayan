c1d371938744eeee870394b9b1e138cc54e18c36
Ashutoshsingh2003 <101525103+Ashutoshsingh2003@users.noreply.github.com>
2023-08-30 10:32:01 +0530

    UnitTest-1 succesfully passed

--- Satellite.java
+++ Satellite.java
@@ -1,9 +1,21 @@
 /**
  * Satellite
  */
+class Directory {
+    int x,y,z ;
+    String direction ;
+    public Directory(int x , int y , int z , String direction){
+         this.x=x;
+         this.y=y;
+         this.z=z;
+         this.direction = direction ;
+    }
+}
 public class Satellite {
 
     public static void main(String[] args) {
-        
+        int[] startingPosition = {0,0,0};
+        String startingDirection = "N";
+        Directory dir = new Directory(startingPosition[0] , startingPosition[1], startingPosition[2] , startingDirection);
     }
 }
--- SatelliteTest.java
+++ SatelliteTest.java
@@ -1,4 +1,15 @@
-public class SatelliteTest {
+import static org.junit.Assert.assertTrue;
 
-    
+import org.junit.Test;
+
+public class SatelliteTest {
+       
+    @Test
+    public void startingPositionandDirection()
+    {
+        Directory dir = new Directory(0 , 0 ,0, "N");
+        assertTrue((dir.direction.equals("N" )) && (dir.x==0)&& (dir.y==0)&&(dir.z==0));
+        
+
+    }
 }
